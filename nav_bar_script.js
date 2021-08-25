51cc9e8f8d84b24a361b43ad512f5ff36e29ce5d
kkagawa88 <kkagawa@pdx.edu>
2021-08-24 18:59:54 -0700

    nav fix, added content for about me, project format idea

--- nav_bar_script.js
+++ nav_bar_script.js
@@ -1,8 +1,9 @@
 const nav = document.querySelector('nav')
+const main_content= document.querySelector('#main_content')
 
 document.querySelector('#burger').addEventListener('click',(e) => 
 {
   e.currentTarget.classList.toggle("active")
   nav.classList.toggle('show')
-  
+  main_content.classList.toggle('shrink_content')
 })
